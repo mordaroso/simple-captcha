877406c559893f1e91318ed4e95d7e7c0633c875
Pablo Castellazzi <pablo.castellazzi@gmail.com>
2011-04-26 17:41:09 -0300

    Fix: proper image generation in production mode

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -1,17 +1,13 @@
-class SimpleCaptchaController < ActionController::Metal
-  include ActionController::Streaming
+class SimpleCaptchaController < ActionController::Base
   include SimpleCaptcha::ImageHelpers
 
-  # GET /simple_captcha
   def show
-    unless params[:id].blank?
-      send_file(
-        generate_simple_captcha_image(params[:id]),
-        :type => 'image/jpeg',
-        :disposition => 'inline',
-        :filename => 'simple_captcha.jpg')
-    else
-      self.response_body = [404, {"Content-Type" => "text/html"}, ["Not Found"]]
-    end
+    render :status => 404 if params[:id].blank?
+    send_data(
+      File.read(generate_simple_captcha_image(params[:id])),
+      :type => 'image/jpeg',
+      :disposition => 'inline',
+      :filename => 'simple_captcha.jpg'
+    )
   end
 end
