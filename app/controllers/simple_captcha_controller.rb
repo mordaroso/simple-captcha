4ba8569fc34f9155b838b590c43260f8d0fda374
mordaroso <mordaroso@gmail.com>
2011-08-12 11:53:54 +0200

    rescue error if image file is not found

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -2,12 +2,16 @@ class SimpleCaptchaController < ActionController::Base
   include SimpleCaptcha::ImageHelpers
 
   def show
-    render :status => 404 if params[:id].blank?
-    send_data(
-      File.read(generate_simple_captcha_image(params[:id])),
-      :type => 'image/jpeg',
-      :disposition => 'inline',
-      :filename => 'simple_captcha.jpg'
-    )
+    begin
+      render :nothing => true, :status => 404 if params[:id].blank?
+      send_data(
+        File.read(generate_simple_captcha_image(params[:id])),
+        :type => 'image/jpeg',
+        :disposition => 'inline',
+        :filename => 'simple_captcha.jpg'
+      )
+    rescue StandardError => e
+      render :nothing => true, :status => 404
+    end
   end
 end
